0d447f11a3f81468e6e1510e0e43563551f03374
Артем <@->
2016-11-23 16:07:38 +0200

    Учимся работать с переменными, а так же научились выводить русские символы в консоли!!!!!!

---
+++ MyProject/Video_lection2/Main.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<conio.h>
+
+using std::cout;
+using std::endl;
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	// Инициализация переменных
+	int apple;
+	int carrots;
+	int potaetoes;
+	double money;
+	char simbol;
+	// Присваивание переменным значений
+	apple = 3;
+	carrots = 7;
+	potaetoes = 15;
+	money = 17.85;
+	simbol = 'x';
+
+	cout << "Apples =  " << apple << endl;
+	cout << "Carrots = " << carrots << endl;
+	cout << "Potaetoes = " << potaetoes << endl;
+	cout << "ПРиивеееет\n";
+	_getch();
+	
+	return 0;
+}
