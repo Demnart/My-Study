e22a796a029d3f927e3b2663080f3a61c0e92059
Артем <@->
2016-12-05 17:18:50 +0200

    Ввод значения с клавиатуры, а так же проверка на символы

--- MyProject/Arrays/Main.cpp
+++ MyProject/Arrays/Main.cpp
@@ -6,8 +6,8 @@ using namespace std;
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-	const int A = 10;
-	int Array[A];
+    
+	int Array[10];
 	//cout << sizeof(Array) << endl;
 	/*Array[0] = 15;
 	Array[1] = 21;
@@ -18,7 +18,18 @@ int main()
 		Array[i] = i + 1;
 		cout << "A[" << i << "] = " << Array[i] << endl;
 	}*/
-
+	for(int i= 0;i<10;i++)
+	{
+		cout << "¬ведите значение "<< i <<" элемента массива ";
+		while (!(cin >> Array[i]))
+		{
+			cin.clear();
+			while (cin.get() != '\n');
+			cout << "¬ведите значение " << i << " элемента массива ";
+		}
+		
+	}
+	
 
 	_getch();
 	return 0;
