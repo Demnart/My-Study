cbfbd52bd354490366aab8e8a88c545009df4446
Артем <@->
2016-12-08 18:10:52 +0200

    чтото

---
+++ MyProject/String/Source.cpp
@@ -0,0 +1,17 @@
+#include<conio.h>
+#include<iostream>
+#include<cstring>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	
+	char symbol[] = "Hello";
+	cout << strlen(symbol)<<endl;
+	_getch();
+	return 0;
+
+
+}
