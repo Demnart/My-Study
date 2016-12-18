9d0e44c1dd6ae07a4d3f264f33baef0d12df8f26
Артем <@->
2016-12-18 14:28:50 +0200

    Функция которая работает без обьявления в начале т.к. обьявлена до функции main

---
+++ LessonsWithCV/Function2/Source.cpp
@@ -0,0 +1,23 @@
+#include<conio.h>
+#include<iostream>
+
+using namespace std;
+
+void Look()
+{
+	cout << "Hey i write first c++ programm with using unother function" << endl;
+	cout << "Now we will see operation 5 + 7  = " << 5 + 7 << endl;
+	cout << "Now we will see operation 5 * 7  = " << 5 * 7 << endl;
+	cout << "Now we will see operation 5 - 7  = " << 5 - 7 << endl;
+	cout << "Now we will see operation 5 / 7  = " << 5 / 7 << endl;
+	cout << "Now we will see operation 5 % 7  = " << 5 % 7 << endl;
+}
+
+int main()
+{
+	Look();
+	_getch();
+	return 0;
+
+}
+
