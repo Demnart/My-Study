8d8797e77032d4076c9dbec5414c1d61d75c45f0
Артем <@->
2016-12-18 14:24:53 +0200

    system files

---
+++ LessonsWithCV/Function/Source.cpp
@@ -0,0 +1,24 @@
+#include<conio.h>
+#include<iostream>
+
+using namespace std;
+
+int Look();
+
+int main()
+{
+	Look();
+	_getch();
+	return Look();
+}
+
+int Look()
+{
+	cout << "Hey i write first c++ programm with using unother function" << endl;
+	cout << "Now we will see operation 5 + 7  = " << 5 + 7 << endl;
+	cout << "Now we will see operation 5 * 7  = " << 5 * 7 << endl;
+	cout << "Now we will see operation 5 - 7  = " << 5 - 7 << endl;
+	cout << "Now we will see operation 5 / 7  = " << 5 / 7 << endl;
+	cout << "Now we will see operation 5 % 7  = " << 5 % 7 << endl;
+	return 0;
+}
