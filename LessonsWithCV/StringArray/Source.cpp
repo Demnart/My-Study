106f2d30bc08088f071c314a868668d565d95a56
Артем <@->
2016-12-19 21:46:25 +0200

    Массив строк с неправильным выводом

---
+++ LessonsWithCV/StringArray/Source.cpp
@@ -0,0 +1,34 @@
+#include<conio.h>
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+	const int ARRAY_SIZE = 10;
+	string massiv[ARRAY_SIZE];
+
+	for (int i = 0; i < ARRAY_SIZE; i++)
+	{
+		cout << "Enter your " << i+1 << " word ";
+		while (!(cin >> massiv[i]))
+		{
+			cin.clear();
+			while (cin.get() != '\n');
+			cout << "Enter your " << i + 1 << " word ";
+		}
+	}
+	system("cls");
+	cout << "\t\t\t***Your massive***\n";
+
+	for (int i = 0; i < ARRAY_SIZE; i++)
+	{
+		cout << "Our array[" << i << "] = " << massiv[i] << " and have " << massiv[i].size() << " letters" << endl;
+	}
+
+
+
+	_getch();
+	return 0;
+}
