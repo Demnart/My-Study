2281fd3ebc3b2f678423b11d6a884b3d72328af9
Артем <@->
2016-12-20 11:00:10 +0200

    Вложенные if

---
+++ LessonsWithCV/Listing6.3/Source.cpp
@@ -0,0 +1,37 @@
+#include<conio.h>
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Enter two numbers\n";
+	float num1 = 0, num2 = 0;
+	cin >> num1;
+	cin >> num2;
+
+	cout << "Enter \'d\' to divide, anything else to multiply:  " << endl;
+	char userInput;
+	cin >> userInput;
+
+	if (userInput == 'd')
+	{
+		cout << "You want to division!" << endl;
+		if (num2 != 0)
+		{
+			cout << "No div-by-zero, processing to calculate" << endl;
+			cout << num1 << " / " << num2 << " = " << num1 / num2 << endl;
+		}
+		else
+		{
+			cout << "Division by zero is not allowed" << endl;
+		}
+	}
+	else
+	{
+		cout << "You want multiplication!!!" << endl;
+		cout << num1 << " * " << num2 << " = " << num1*num2 << endl;
+	}
+	_getch();
+	return 0;
+}
