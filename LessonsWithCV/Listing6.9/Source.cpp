d453a690d02ad2bcf7f804b836e17ecbbbcc8fd7
Артем <@->
2016-12-20 14:43:09 +0200

    Изменения для предотвращения ошибок при вводе символов

--- LessonsWithCV/Listing6.9/Source.cpp
+++ LessonsWithCV/Listing6.9/Source.cpp
@@ -11,16 +11,19 @@ int main()
 	{
 		int num = 0, num1 = 0;
 		cout << "Enter two numbers\n";
-		cin >> num;
-		cin >> num1;
-
-		cout << num << " + " << num1 << " = " << num + num1 << endl;
-		cout << num << " * " << num1 << " = " << num * num1 << endl;
-
-		cout << "Do you want to do one more time?(y/n)" << endl;
+		while (!(cin >> num) || !(cin >> num1))
+		{
+			cin.clear();
+			while (cin.get() != '\n');
+		}
 		
-		cin >> userSelection;
+			cout << num << " + " << num1 << " = " << num + num1 << endl;
+			cout << num << " * " << num1 << " = " << num * num1 << endl;
 
+			cout << "Do you want to do one more time?(y/n)" << endl;
+
+			cin >> userSelection;
+		
 	} while (userSelection == 'y');
 	cout << "GoodBye!!!";
 	_getch();
