021152b3409d346f27fd6cd47937f1f990ce0f1e
Артем <@->
2016-12-21 14:56:37 +0200

    Добавлены коментарии

--- LessonsWithCV/Listing7.4DefaultParametr/Source.cpp
+++ LessonsWithCV/Listing7.4DefaultParametr/Source.cpp
@@ -2,38 +2,38 @@
 #include<iostream>
 
 using namespace std;
-
-double area(double userInput, double pi = 3.14);
+//Обьявление функции(Прототип)
+double area(double userInput, double pi = 3.14);//Число пи в данном случае будте дефолтным параметром
 
 int main()
 {
 	double radius=0;
 	cout << "Enter your radius ";
 	cin >> radius;
-
+	//Условия изменения числа пи
 	cout << "\nPi is a 3.14 do you want to change it?(y/n) ";
 	char userAnswer = '\0';
 	cin >> userAnswer;
 
 	double areaOfCirle= 0 ;
 
-	if (userAnswer == 'y')
+	if (userAnswer == 'y')//Если польователь желает изменить число пи
 	{
-		double newPi = 0;
+		double newPi = 0;//создание новой переменной хранящей будущее значение числа пи
 		cout << "Enter your new Pi ";
 		cin >> newPi;
-		areaOfCirle = area(radius, newPi);
+		areaOfCirle = area(radius, newPi);//передача аргументов функции 
 	}
-	else 
+	else //если пользователь не желает менять число пи
 	{
-		areaOfCirle = area(radius);
+		areaOfCirle = area(radius);//передается только основной аргумент
 	}
 
 	cout << "Your Area is " << areaOfCirle << endl;
 	_getch();
 	return 0;
 }
-
+//Вызов функции
 double area(double userInputRadius, double pi)
 {
 	return userInputRadius*userInputRadius*pi;
